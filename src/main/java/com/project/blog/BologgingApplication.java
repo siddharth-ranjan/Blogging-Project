4b7cdbad635da5b674726a036c4d11bde793f64b
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2024-01-25 23:31:33 +0530

    Initial Commit

---
+++ src/main/java/com/project/blog/BologgingApplication.java
@@ -0,0 +1,13 @@
+package com.project.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BologgingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BologgingApplication.class, args);
+    }
+
+}
